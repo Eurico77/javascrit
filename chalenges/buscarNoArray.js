c573bc03b5c58937ff2e23a89cd44d2502b33a72
eurico77 <euricomagalhaes61@gmail.com>
2021-01-04 09:54:33 -0300

    searchByCategorie

---
+++ chalenges/buscarNoArray.js
@@ -0,0 +1,30 @@
+
+
+
+
+const booksByCategories = [
+   {
+      category: 'Ruiqueza',
+      books: [
+         {
+            title: 'Os segredos da mente milionaria',
+            author: 'T. Harv Eker'
+         },
+         {
+            title: 'Os segredos da mente milionaria',
+            author: 'T. Harv Eker'
+         },
+         {
+            title: 'Os segredos da mente milionaria',
+            author: 'T. Harv Eker'
+         }
+
+      ]
+   },
+   {
+      category: 'Inteligencia emocional',
+      books: [
+         
+      ]
+   }
+]
